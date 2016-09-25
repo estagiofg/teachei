ec1ef74bb46f80a736dfc4bf235a6b34098fe84c
Taillan573 <tailan235@gmail.com>
2016-09-25 17:09:00 -0300

    Add files via upload

---
+++ Teachei/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <title>TeacheiFg </title>
+    </head>
+<?php
+   require 'vendor/autoload.php';
+   $controller = new App\Mvc\Controller();
+   $controller->index();
